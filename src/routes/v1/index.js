92e6e697d36f35ace58c78464ad96ad504b78996
duycuonggg <thivip201197@gmail.com>
2025-10-15 17:18:01 +0700

    config_server

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,5 +1,9 @@
-/**
- * Updated by trungquandev.com's author on August 17 2023
- * YouTube: https://youtube.com/@trungquandev
- * "A bit of fragrance clings to the hand that gives flowers!"
- */
+import express from 'express'
+import { StatusCodes } from 'http-status-codes'
+
+const Router = express.Router()
+Router.get('/status', (req, res) => {
+  res.status(StatusCodes.OK).json({ message: 'APIs_V1 are ready to use' })
+})
+
+export const APIs_V1 = Router
